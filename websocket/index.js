265717a01bdfe86edbef008f8d99b31bc1fa0455
Nico Aßfalg <nico.assfalg@student.kit.edu>
2021-01-24 22:27:16 +0100

    Websocket: Einfachen Server erstellen

--- websocket/index.js
+++ websocket/index.js
@@ -1,3 +1,11 @@
-const ws = require('ws');
+const WebSocket = require('ws');
 
-// TODO: Websocket...
+const wss = new WebSocket.Server({ port: 8081 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+    ws.send('something');
+  });
+
+});
